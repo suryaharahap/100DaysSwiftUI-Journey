ae16228065bd43388b9710be089af1eb21303280
suryaharahap <suryaharahap18@gmail.com>
2023-01-11 23:59:16 +0700

    Learn basic variabel, constanta and string

---
+++ 01-learning/MyPlaygroundDay01.playground/Contents.swift
@@ -0,0 +1,49 @@
+import Cocoa
+
+// creating variable
+var name = "Surya"
+name = "Harahap"
+name = "Aaj"
+
+var playerName = "Arif"
+print(playerName)
+
+playerName = "Nanda"
+print(playerName)
+
+playerName = "Dedek"
+print(playerName)
+
+// creating constanta and multi-line strings
+let movie = """
+A day in
+the life of an
+Software Engineer (iOS)
+"""
+
+let managerName = "Sky lar"
+let myMentor = "Mas isa"
+let meaningOfLife = "How many roads must a man walk down?"
+
+print(myMentor.count)
+
+let namelength = managerName.count
+print(namelength)
+print(myMentor.uppercased())
+print(meaningOfLife.hasSuffix("Hw many roads"))
+print(meaningOfLife.hasSuffix("How many roads must a man walk down?"))
+print(managerName.hasSuffix("sky lar"))
+print(managerName.hasSuffix("Sky lae"))
+
+
+let score = 10
+let reallyBig = 1_000_00_0000
+
+let lowerScore = score - 2
+let higherScore = score + 10
+let doubledScore = score * 2
+let squaredScore = score * score
+let halvedScore = score / 2
+print(score)
+
+
